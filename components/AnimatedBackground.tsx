daf4924b7127d999db4e2172e9b8168fb0891dd1
Ashish Nagvanshi <aashishsingh24032005@gmail.com>
2026-02-03 20:33:54 +0530

    fixed taht chnage s

--- components/AnimatedBackground.tsx
+++ components/AnimatedBackground.tsx
@@ -13,7 +13,7 @@ interface AnimatedBackgroundProps {
 }
 
 const AnimatedBackground = ({
-    color = "#00ff00", // Neon Green
+    color = "#00ff00", // Neon Gree
     particleCount = 80,
     speed = 1.2,
     lineOpacity = 0.5,
